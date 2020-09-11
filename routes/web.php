3169b3fc36848e88a2a1a09d6d48cc830ea8731b
YeWinNaing-99 <70206025+YeWinNaing-99@users.noreply.github.com>
2020-09-12 04:24:25 +0630

    This is route testing by YWN

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+	//route testing by Ye Win Naing
+Route::get('/testing',function($value=''){
+	return 'This is route testing';
+});
